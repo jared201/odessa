9d4b8b722ecb1784be22cf7a71953167a4d0b456
Jared Odulio <jared.odulio@gmail.com>
2018-08-01 17:51:21 +0800

    added bulma

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+let sass = require('sass');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +9,9 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+sass.render({file: 'public/stylesheets/styles.scss'}, function(err, result) {
+    if (err) throw err;
+     console.log("css", result);
+
+/* ... */ });
