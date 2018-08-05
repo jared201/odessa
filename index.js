e5f6e9f162e652e9d1b7182ab9beb15cb27729c8
Jared Odulio <jared.odulio@gmail.com>
2018-08-05 17:45:51 +0800

    added the swag prototype

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const swag = require ('./src/swag_module');
 let sass = require('sass');
 
 express()
@@ -8,6 +9,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/test_modules', (req, response) => {
+
+       swag.getEMAdata('JFC', (sym, res)=>{
+         console.log("RESULT: ", sym + ":" + res);
+       });
+       response.status(200).send('ok');
+  } )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 sass.render({file: 'public/stylesheets/styles.scss'}, function(err, result) {
