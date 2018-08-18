6cd4cd2e5e5639ae388159f37a1202299d215efd
Jared Odulio <jared.odulio@gmail.com>
2018-08-18 15:05:37 +0800

    added semantic-ui

--- src/swag_module.js
+++ src/swag_module.js
@@ -5,6 +5,8 @@ exports.getSMAdata = function (symbol, callback){
 
      let result = 0;
      /*TODO implement the HTTP GET API from PSE for extracting SMA of a stock*/
+     /* Closing prices for 20 day period divided by 20
+     */
      callback (symbol, result);
      return result;
 }
@@ -12,7 +14,9 @@ exports.getSMAdata = function (symbol, callback){
 exports.getEMAdata = function (symbol, callback){
 
      let result = 0;
-
+     /*
+      * get SMA then the multiplier 18.18
+     */
      callback (symbol, result);
      return result;
 
