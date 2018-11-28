60641aa09c459a1b46b3dd54924ce5104bebbfda
Jared Odulio <jared.odulio@salesfix.com.au>
2018-11-28 09:19:17 +0800

    changed format to quiz

--- public/js/actions.js
+++ public/js/actions.js
@@ -1,3 +1,9 @@
 function submitStock() {
   $('.modal').toggleClass('is-active');
 }
+
+$('button').click(function(){
+    $('button').toggleClass('active');
+    $('.title').toggleClass('active');
+    $('nav').toggleClass('active');
+});
